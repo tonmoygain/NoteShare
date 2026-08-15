482d7b5717a8719382d8560f2903e3f15f6510d6
tonmoygain <tonmoygain123@gmail.com>
2026-08-16 01:25:20 +0600

    Prepare NoteShare for production deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "https://noteshare-uy4z.onrender.com/api/",
 });
 
 
@@ -147,7 +147,7 @@ API.interceptors.response.use(
 
             const response =
                 await axios.post(
-                    "http://127.0.0.1:8000/api/token/refresh/",
+                    "https://noteshare-uy4z.onrender.com/api/token/refresh/",
                     {
                         refresh,
                     }
